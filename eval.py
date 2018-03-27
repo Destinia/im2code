0b33d1c9ae0b7bfe1ba1151ba7932663a71de427
Allen Lin <allen830423@gmail.com>
2018-03-27 20:55:51 +0800

    complete evaluation

--- eval.py
+++ eval.py
@@ -1,48 +1,56 @@
 import torch
 import os
+from tqdm import tqdm
 from options.test_options import TestOptions
 from dataloader import UI2codeDataloader
 from torch.autograd import Variable
 
 from models.model import EncoderCNN, EncoderRNN, AttentionDecoder
 import pickle
-from eval_utils import wordErrorRate
+from eval_utils import wordErrorRate, TreeEditDistance
 
 def main(opt):
-    data_loader = UI2codeDataloader(opt, phase='train')
-    opt.vocab = data_loader.get_vocab()
-    opt.rev_vocab = [t[0]
-                    for t in list(sorted(opt.vocab.items(), key=lambda x: x[1]))]
+    data_loader = UI2codeDataloader(opt, phase='test')
     opt.target_vocab_size = len(opt.vocab)
+    treeEval = TreeEditDistance(opt)
     dataset = data_loader.load_data()
     dataset_size = len(data_loader)
     encoderCNN = EncoderCNN(opt)
     encoderRNN = EncoderRNN(opt)
     decoder = AttentionDecoder(opt)
     encoderCNN.load_state_dict(torch.load(os.path.join(
-        opt.results_dir, 'encoder-cnn-%d.pkl' % (opt.model_iter))))
+        opt.results_dir, 'encoder-cnn-%s.pkl' % (opt.model_name))))
     encoderRNN.load_state_dict(torch.load(os.path.join(
-        opt.results_dir, 'encoder-rnn-%d.pkl' % (opt.model_iter))))
+        opt.results_dir, 'encoder-rnn-%s.pkl' % (opt.model_name))))
     decoder.load_state_dict(torch.load(os.path.join(
-        opt.results_dir, 'decoder-%d.pkl' % (opt.model_iter))))
+        opt.results_dir, 'decoder-%s.pkl' % (opt.model_name))))
 
     if torch.cuda.is_available():
         encoderCNN.cuda()
         encoderRNN.cuda()
         decoder.cuda()
-    for (images, captions, masks) in data_loader:
+    accuracy_beams, accuracy_greedies = [], []
+    for (images, captions, masks) in tqdm(data_loader):
         images = Variable(images, requires_grad=False).cuda()
         captions = Variable(captions, requires_grad=False).cuda()
         masks = Variable(masks, requires_grad=False).cuda()
 
         features = encoderCNN(images)
         encoded_features = encoderRNN(features)
-        output_greedy = decoder.decoder(encoded_features)
+        output_greedy = decoder.decode(encoded_features)
         beam_output, beam_score = decoder.decode_beam(encoded_features)
-        accuracy = wordErrorRate(
-            output_greedy[:, :-1], captions.data[:, 1:], opt.rev_vocab)
-        print(accuracy)
-
+        accuracy_beam = wordErrorRate(
+            beam_output, captions.data[:, 1:], opt.rev_vocab)
+        accuracy_greedy = wordErrorRate(
+            output_greedy, captions.data[:, 1:], opt.eos)
+        # accuracy_tree = treeEval.distance(beam_output, captions.data[:, 1:])
+        accuracy_greedies.append(accuracy_greedy)
+        accuracy_beams.append(accuracy_beam)
+        # print(accuracy_beam, accuracy_greedy)
+        # print(accuracy_greedy, accuracy_beam, accuracy_tree)
+        # print(accuracy_beam, accuracy_greedy)
+    print('accuracy beam search: %.4f'%(sum(accuracy_beams)/len(accuracy_beams)))
+    print('accuracy greedy search: %.4f'%(sum(accuracy_greedies)/len(accuracy_greedies)))
 if __name__ == '__main__':
     opt = TestOptions().parse()
     main(opt)
--- eval_utils.py
+++ eval_utils.py
@@ -1,17 +1,55 @@
 from Levenshtein import distance
+from zss import Node, simple_distance
 
 
 
-def wordErrorRate(results, targets, vocab):
+def wordErrorRate(results, targets, eos_index):
     def tokens2str(tokens):
         ret = ''
         for t in tokens:
-            if vocab[int(t)] == '</s>':
+            if int(t) == eos_index:
                 break
-            ret = ret + vocab[int(t)] + ' '
+            ret = ret + chr(int(t)) + ' '
         return ret
     results_str = [tokens2str(r, ) for r in results]
     targets_str = [tokens2str(r) for r in targets]
 
     edit_distance = [min(1, distance(r, t)/len(t)) for r, t in zip(results_str, targets_str)]
     return 1 - sum(edit_distance)/len(edit_distance)
+
+
+class TreeEditDistance():
+    def __init__(self, opt):
+        self.vocab = opt.rev_vocab
+        self.opening_tag = opt.opening_tag
+        self.closing_tag = opt.closing_tag
+        self.eos = '</s>'
+
+    def build_tree(self, seq):
+
+        root = Node('root')
+        hierarchy = [root, None] # 0 for parent 1 for current node
+        for t in seq:
+            token = self.vocab[t]
+            if token == self.opening_tag:
+                hierarchy.append(None)
+                continue
+            if token == self.closing_tag:
+                del hierarchy[-1]
+                continue
+            if token == self.eos:
+                break
+            newNode = Node(token)
+            hierarchy[-2].addkid(newNode)
+            hierarchy[-1] = newNode
+        return root
+
+    def distance(self, result, target):
+        try:
+            return simple_distance(self.build_tree(result), self.build_tree(target))
+        except:
+            return 0 ## build tree error penalty
+
+    def distance(self, results, targets):
+        distances = [self.distance(r, t)/len(t) for r, t in zip(results, targets)]
+        return sum(distances)/len(distances)
